71797994bf7dafeae1721cb3c09fae5444624a26
Abdan Syakuro <abdan.syakuro@students.amikom.ac.id>
2021-09-21 08:44:17 +0800

    first commit

---
+++ app/Http/Controllers/HewanController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use App\Models\Hewan;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class HewanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $datahwn = Hewan::with('customer')->paginate(10);
+        return view('hewan.data-hewan', compact('datahwn'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $cust = Customer::all();
+        return view('hewan.create-hewan',compact('cust'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Hewan::create([
+            'namahewan' => $request->namahewan,
+            'jenishewan' => $request->jenishewan,
+            'customer_id' => $request->customer_id,
+        ]);
+
+        return redirect('/hewan/data-hewan')->with('toast_success', 'Data Berhasil Disimpan');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $cust = Customer::all();
+        $hwn = Hewan::with('customer')->findorfail($id);
+        return view('/hewan/edit-hewan',compact('hwn','cust'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $hwn = Hewan::findorfail($id);
+        $hwn->update($request->all());
+
+        return redirect('/hewan/data-hewan')->with('toast_success', 'Data Berhasil Diubah');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $hwn = Hewan::findorfail($id);
+        $hwn->delete();
+        return back()->with('info', 'Data Berhasil Dihapus');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('beranda.beranda');
+});
+
+Route::get('/customer/data-customer','App\Http\Controllers\CustomerController@index')->name('data-customer');
+Route::get('/customer/create-customer','App\Http\Controllers\CustomerController@create')->name('create-customer');
+Route::post('/customer/simpan-customer','App\Http\Controllers\CustomerController@store')->name('simpan-customer');
+Route::get('/customer/edit-customer/{id}','App\Http\Controllers\CustomerController@edit')->name('edit-customer');
+Route::post('/customer/update-customer/{id}','App\Http\Controllers\CustomerController@update')->name('update-customer');
+Route::get('/customer/delete-customer/{id}','App\Http\Controllers\CustomerController@destroy')->name('deletephp a-customer');
+
+Route::get('/hewan/data-hewan','App\Http\Controllers\HewanController@index')->name('data-hewan');
+Route::get('/hewan/create-hewan','App\Http\Controllers\HewanController@create')->name('create-hewan');
+Route::post('/hewan/simpan-hewan','App\Http\Controllers\HewanController@store')->name('simpan-hewan');
+Route::get('/hewan/edit-hewan/{id}','App\Http\Controllers\HewanController@edit')->name('edit-hewan');
+Route::post('/hewan/update-hewan/{id}','App\Http\Controllers\HewanController@update')->name('update-hewan');
+Route::get('/hewan/delete-hewan/{id}','App\Http\Controllers\HewanController@destroy')->name('deletephp a-hewan');
+
+Route::get('/barang/data-barang','App\Http\Controllers\BarangController@index')->name('data-barang');
+Route::get('/barang/create-barang','App\Http\Controllers\BarangController@create')->name('create-barang');
+Route::post('/barang/simpan-barang','App\Http\Controllers\BarangController@store')->name('simpan-barang');
+Route::get('/barang/edit-barang/{id}','App\Http\Controllers\BarangController@edit')->name('edit-barang');
+Route::post('/barang/update-barang/{id}','App\Http\Controllers\BarangController@update')->name('update-barang');
+Route::get('/barang/delete-barang/{id}','App\Http\Controllers\BarangController@destroy')->name('deletephp a-barang');
+
+Route::get('/grooming/data-grooming','App\Http\Controllers\GroomingController@index')->name('data-grooming');
+Route::get('/grooming/create-grooming','App\Http\Controllers\GroomingController@create')->name('create-grooming');
+Route::post('/grooming/simpan-grooming','App\Http\Controllers\GroomingController@store')->name('simpan-grooming');
+Route::get('/grooming/edit-grooming/{id}','App\Http\Controllers\GroomingController@edit')->name('edit-grooming');
+Route::post('/grooming/update-grooming/{id}','App\Http\Controllers\GroomingController@update')->name('update-grooming');
+Route::get('/grooming/delete-grooming/{id}','App\Http\Controllers\GroomingController@destroy')->name('deletephp a-grooming');
+
+Route::get('/penyakit/data-penyakit','App\Http\Controllers\PenyakitController@index')->name('data-penyakit');
+Route::get('/penyakit/create-penyakit','App\Http\Controllers\PenyakitController@create')->name('create-penyakit');
+Route::post('/penyakit/simpan-penyakit','App\Http\Controllers\PenyakitController@store')->name('simpan-penyakit');
+Route::get('/penyakit/edit-penyakit/{id}','App\Http\Controllers\PenyakitController@edit')->name('edit-penyakit');
+Route::post('/penyakit/update-penyakit/{id}','App\Http\Controllers\PenyakitController@update')->name('update-penyakit');
+Route::get('/penyakit/delete-penyakit/{id}','App\Http\Controllers\PenyakitController@destroy')->name('deletephp a-penyakit');
+
+Route::get('/grooming/data-layanan-grooming','App\Http\Controllers\LayananGroomingController@index')->name('data-layanan-grooming');
+Route::get('/grooming/create-layanan-grooming','App\Http\Controllers\LayananGroomingController@create')->name('create-layanan-grooming');
+Route::post('/grooming/simpan-layanan-grooming','App\Http\Controllers\LayananGroomingController@store')->name('simpan-layanan-grooming');
+Route::get('/grooming/edit-layanan-grooming/{id}','App\Http\Controllers\LayananGroomingController@edit')->name('edit-layanan-grooming');
+Route::post('/grooming/update-layanan-grooming/{id}','App\Http\Controllers\LayananGroomingController@update')->name('update-layanan-grooming');
+Route::get('/grooming/delete-layanan-grooming/{id}','App\Http\Controllers\LayananGroomingController@destroy')->name('deletephp a-layanan-grooming');
